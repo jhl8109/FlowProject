eb6d89c992a11fddcd194f4ad42de0d6102b2d9f
제호 이 <jhl81094@gmail.com>
2021-12-31 15:16:53 +0900

    new start

--- FlowProject/app/src/main/java/com/example/flowproject/dataVo.kt
+++ FlowProject/app/src/main/java/com/example/flowproject/dataVo.kt
@@ -2,4 +2,4 @@ package com.example.flowproject
 
 /*dataVo.kt*/
 //class DataVo(val name: String, val id: String, val address: String, val pay: String, val photo: String)
-class DataVo(var name: String, var address: String, var phonenumber: String, val photo: String)
+class DataVo(val name: String, val address: String, val phonenumber: String, val photo: String)
